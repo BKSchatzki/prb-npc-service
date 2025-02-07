9b33eb2be405ba62ff2d0e42d1dd2981d368c503
Brendan K. Schatzki <106890912+BKSchatzki@users.noreply.github.com>
2025-02-06 22:29:36 -0800

    Explicitly type request and response

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ import express, {
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (req: Request, res: Response) => {
   res.json({ helloWorld: 'Hello, World!' });
 });
 
